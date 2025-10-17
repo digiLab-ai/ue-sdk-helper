1c4324a9c5bc6d67b43caf7dfddc4ab28624e5ab
danielgreenhouse <dmgreenhouse@outlook.com>
2025-10-17 14:18:06 +0100

    Update train and save workflow

--- src/ue_helper/workflows.py
+++ src/ue_helper/workflows.py
@@ -5,6 +5,12 @@ from uncertainty_engine.nodes.base import Node
 from uncertainty_engine.graph import Graph
 from uncertainty_engine.nodes.workflow import Workflow
 
+from ue_helper.utils import (
+    get_project_id,
+    get_resource_id,
+    wrap_resource_id,
+)
+
 def train_and_save_model_workflow(client, 
                    project_name: str,
                    dataset_name: str, 
@@ -16,54 +22,69 @@ def train_and_save_model_workflow(client,
     """
     A workflow that trains a machine learning model.
     Here, we assume all resources have already been uploaded to the cloud.
-    Simplified methods exist for local data.
+    :param client: The Uncertainty Engine client.
+    :param project_name: The name of the project.
+    :param dataset_name: The name of the dataset.
+    :param input_names: The names of the input columns.
+    :param output_names: The names of the output columns.
+    :param save_model_name: The name to save the trained model as.
+    :param is_visualise_workflow: Whether to print the workflow graph.
+    :param is_print_full_output: Whether to print the full output of the workflow.
+    :return: The response from running the workflow.
     """
-    # Create the graph
+    # 1. Create the graph
     graph = Graph()
 
-    # Create relevant nodes:
+    # 2. Create relevant nodes, handles, and add to graph:
 
-    # Define the model config node
+    # 2.a. Model config node
     model_config = Node(
         node_name="ModelConfig",
         label="Model Config",
     )
-    graph.add_node(model_config)
-    # Add a handle to the the config output
-    output_config = model_config.make_handle("config")
-
-    # Define the load dataset node
+    graph.add_node(model_config)  # add to graph
+    output_config = model_config.make_handle("config")  # add handle
+    
+    # 2.b. Load dataset node
     load_data = Node(
         node_name="LoadDataset",
         label="Load Dataset",
-        file_id=get_resource_id(client=client, project_name=project_name, resource_name=dataset_name),
-        project_id=get_project_id(client=client, project_name=project_name),
+        file_id=wrap_resource_id(
+            get_resource_id(
+                client=client,
+                project_name=project_name, 
+                resource_name=dataset_name, 
+                resource_type='dataset')
+                ),
+        project_id=get_project_id(
+            client=client,
+            project_name=project_name
+            ),
     )
-    graph.add_node(load_data)
-    dataset = load_data.make_handle("dataset")
-
-    # Define the input filter dataset node
+    graph.add_node(load_data)  # add to graph
+    dataset = load_data.make_handle("file")  # add handle
+    
+    # 2.b. Filter dataset node for inputs
     input_data = Node(
         node_name="FilterDataset",
         label="Input Dataset",
         columns=input_names,
         dataset=dataset,
     )
-    graph.add_node(input_data)
-    input_dataset = load_data.make_handle("input_dataset")
-
-    # Define the output filter dataset node
+    graph.add_node(input_data)  # add to graph
+    input_dataset = input_data.make_handle("dataset")  # add handle
+    
+    # 2.c. Filter dataset node for outputs
     output_data = Node(
         node_name="FilterDataset",
         label="Output Dataset",
         columns=output_names,
         dataset=dataset,
     )
-    graph.add_node(output_data)
-    output_dataset = load_data.make_handle("output_dataset")
-
-
-    # Define the train model node
+    graph.add_node(output_data)  # add to graph
+    output_dataset = output_data.make_handle("dataset")  # add handle
+    
+    # 2.d. Train model node
     train_model = Node(
         node_name="TrainModel",
         label="Train Model",
@@ -71,16 +92,21 @@ def train_and_save_model_workflow(client,
         inputs=input_dataset,
         outputs=output_dataset,
     )
-    graph.add_node(train_model)
-    # Add a handle to the the config output
-    output_model = train_model.make_handle("model")
+    graph.add_node(train_model)  # add to graph
+    output_model = train_model.make_handle("model")  # add handle
+    
+    # 2.e. Save model node
     save = Node(
         node_name="Save",
         label="Save",
-        file=output_model,
-        fileid=save_model_name,
+        data=output_model,
+        file_id=save_model_name,
+        project_id=get_project_id(
+            client=client,
+            project_name=project_name
+            ),
     )
-    graph.add_node(save)
+    graph.add_node(save)  # add to graph
 
     if is_visualise_workflow:
         pprint(graph.nodes)
